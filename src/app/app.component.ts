2e7c3706e80915ca220067d24351efb6c23d1dd4
ncrown1190 <ncrown88@outlook.com>
2025-01-25 22:38:18 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'to-do-list';
+  // title = 'to-do-list';
+  todos: Todo[] = [
+    { task: 'Buy Grocery', completed: false, duration: 1 },
+    { task: 'Laundry', completed: true, duration: 2 },
+    { task: 'Walk the dog', completed: true, duration: 0.45 },
+    { task: 'Call Mom', completed: false, duration: 1 },
+    { task: 'relax', completed: false, duration: 1.5 },
+  ];
+
+  onComplete(index: number): void {
+    this.todos[index].completed = true;
+  }
+
+  onDelete(index: number): void {
+    this.todos.splice(index, 1);
+  }
 }
