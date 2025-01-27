7b8a731082db038412f96ecbdb6e873691f503ae
ncrown1190 <ncrown88@outlook.com>
2025-01-27 17:06:29 -0500

    priority levels add

--- src/app/Todo.interface.ts
+++ src/app/Todo.interface.ts
@@ -1,5 +1,6 @@
-export interface Todo{
-    task: string;
-    completed: boolean;
-    duration: number;    
+export interface Todo {
+  task: string;
+  completed: boolean;
+  duration: number;
+  priority: 'Low' | 'Normal' | 'High';
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,17 @@ import { Todo } from './Todo.interface';
 export class AppComponent {
   // title = 'to-do-list';
   todos: Todo[] = [
-    { task: 'Buy Grocery', completed: false, duration: 1 },
-    { task: 'Laundry', completed: true, duration: 2 },
-    { task: 'Walk the dog', completed: true, duration: 0.45 },
-    { task: 'Call Mom', completed: false, duration: 1 },
-    { task: 'relax', completed: false, duration: 1.5 },
+    { task: 'Buy Grocery', completed: false, duration: 1, priority: 'Normal' },
+    { task: 'Laundry', completed: true, duration: 2, priority: 'Low' },
+    { task: 'Walk the dog', completed: true, duration: 0.45, priority: 'High' },
+    { task: 'Call Mom', completed: false, duration: 1, priority: 'High' },
+    { task: 'relax', completed: false, duration: 2.0, priority: 'Normal' },
+    {
+      task: 'watching movie',
+      completed: false,
+      duration: 1.5,
+      priority: 'Low',
+    },
   ];
 
   onComplete(index: number): void {
